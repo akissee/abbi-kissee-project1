ee493eacfd9576812f537f916c1ab335f6200986
Abbi Kissee <abbikissee@Abbis-MBP.attlocal.net>
2022-11-30 13:34:14 -0600

    Edited Index Page Blog Posts

--- blog/gatsby-node.js
+++ blog/gatsby-node.js
@@ -1,9 +1,35 @@
-exports.createPages = async ({ actions }) => {
+
+const path = require(`path`);
+
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allContentfulBlogPost {
+          edges {
+            node {
+              id 
+              slug 
+            }
+          }
+        }
+      }
+    `).then(result => {
+      if (result.errors) {
+        reject(result.errors)
+      }
+      result.data.allContentfulBlogPost.edges.forEach((edge) => {
+        createPage({
+          path: edge.node.slug,
+          component: require.resolve("./src/templates/blog-post.js"),
+          context: {
+            slug: edge.node.slug
+          },
+          defer: true,
+        })
+      })
+      resolve()
+    })
   })
 }
